64a7b48d5055dc0f618ffe54d797055f95f433cf
Narayanan V <vnarayanan2696@gmail.com>
2020-02-06 20:38:46 +0530

    read milage details from db

--- main.go
+++ main.go
@@ -6,12 +6,23 @@ import (
 	"net/http"
 	"trip-planer/controller"
 	"trip-planer/initializers"
+	"trip-planer/model"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	fmt.Println("=======Trip Planer=======")
 	if initializers.InitiateEnv() == true {
 		router := controller.Register()
+		db := model.Connection()
+		fmt.Printf("Db value:%v", &db)
+		err := db.Ping()
+		if err != nil {
+			fmt.Println("connection is not active")
+			log.Fatal(err.Error)
+		}
+		defer db.Close() // defer is used to execute the statement end of the scope here last line of main()
 		http.ListenAndServe("localhost:3000", router)
 	} else {
 		log.Fatal("error in loading env")
--- views/request_response.go
+++ views/request_response.go
@@ -3,4 +3,5 @@ package views
 type TripDetailsRequest struct {
 	Source      string `json:"from"`
 	Destination string `json:"to"`
+	Car         string `json:"car_name"`
 }
