4a7255ac045c3ed8a51a125e5d8cafb022266512
Narayanan2696 <vnarayanan2696@gmail.com>
2021-03-06 13:10:28 +0530

    initiate env var changes

--- initializers/initiate_env.go
+++ initializers/initiate_env.go
@@ -1,13 +1,20 @@
 package initializers
 
 import (
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func InitiateEnv() bool {
-	err := godotenv.Load()
-	if err != nil {
-		return false
+	path, _ := os.Getwd()
+	path = path + "../.env"
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := godotenv.Load()
+		if err != nil {
+			return false
+		}
 	}
+
 	return true
 }
